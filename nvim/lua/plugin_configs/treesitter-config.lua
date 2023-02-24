17f1abfc791f399cc59b6634325e262201f538a2
Rupam Ganguly <hirupamganguly@gmail.com>
2023-02-24 19:44:18 +0530

    Update treesitter-config.lua

--- nvim/lua/plugin_configs/treesitter-config.lua
+++ nvim/lua/plugin_configs/treesitter-config.lua
@@ -1,7 +1,7 @@
 -- This Config is use to setup Formatting code for NEOVIM
 require'nvim-treesitter.configs'.setup {
    -- A list of parser names, or "all" (the four listed parsers should always be installed)
-   ensure_installed = { "c", "lua", "vim", "help", "go" },
+   ensure_installed = { "go" },
 
    -- Install parsers synchronously (only applied to `ensure_installed`)
    sync_install = false,
